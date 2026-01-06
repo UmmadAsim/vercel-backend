09ad1988ca90ebeac1b50975ac370c12eada4977
M Ummad Asim <140070816+UmmadAsim@users.noreply.github.com>
2026-01-06 11:54:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,14 +8,22 @@ import express from 'express'
 import cors from 'cors'
 import { createtodo, deletetodo, fetchtodo, updatetodo } from './controller/todo.controller.js'
 const app = express()
-app.listen(process.env.PORT || 8080)
-
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
+// Root route
+app.get('/', (req, res) => {
+  res.send('Backend is working!');
+})
+
+// API routes
 app.post("/todo", createtodo)
 app.get("/todo", fetchtodo)
 app.put("/todo/:id", updatetodo)
 app.delete("/todo/:id", deletetodo)
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Server running on port ${process.env.PORT || 8080}`);
+});
